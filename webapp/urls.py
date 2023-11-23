9c1ddacc295d68fa23f89a54edd4bb97399813e4
Lobanov Ivan <physic2003@mail.ru>
2023-11-23 15:47:07 +0300

    add order.html and executor.html + finished layout + add routing on new pages

--- webapp/urls.py
+++ webapp/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('executors/', views.executors, name='executors'),
     path('executors/contractors', views.contractors, name='contractors'),
     path('catalog/orders', views.orders, name='orders'),
+    path('catalog/orders/order', views.order, name='order'),
+    path('executors/contractors/contractor', views.contractor, name='contractor'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,17 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# Заказы
+
 def home(req):
     return render(req, 'public/pages/orders_mainPage.html')
 
+
+def orders(req):
+    return render(req, 'public/pages/orders_listing.html')
+
+
+def order(req):
+    return render(req, 'public/order.html')
+
+
+# Исполнители
 
 def executors(req):
     return render(req, 'public/pages/executors_mainPage.html')
 
 
 def contractors(req):
-    return render(req, 'public/executors.html')
+    return render(req, 'public/pages/executors_listing.html')
 
 
-def orders(req):
-    return render(req, 'public/orders.html')
+def contractor(req):
+    return render(req, 'public/executor.html')
+
+
+
