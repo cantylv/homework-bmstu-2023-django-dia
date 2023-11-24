44d0001146ac4a60eadeb872d8c0d095519a15ba
Lobanov Ivan <physic2003@mail.ru>
2023-11-24 03:48:34 +0300

    deep extends and more components | routing for orders | test data in database.py

---
+++ webapp/database.py
@@ -0,0 +1,119 @@
+# Локальная база данных
+
+import random
+
+# Каталоги заказов/исполнителей
+
+CATALOGS = [
+    {
+        'id': 1,
+        "name": "Доставка и логистика",
+        "src": "svg/orders/delivery.svg"
+    },
+    {
+        'id': 2,
+        "name": "Торговля",
+        "src": "svg/orders/trading.svg"
+    },
+    {
+        "id": 3,
+        "name": "Услуги и прочее",
+        "src": "svg/orders/other.svg"
+    },
+    {
+        "id": 4,
+        "name": "Менеджмент, продажи, администрирование",
+        "src": "svg/orders/management.svg"
+    },
+    {
+        "id": 5,
+        "name": "Строительство, ремонт, производство",
+        "src": "svg/orders/construction.svg"
+    },
+
+    {
+        "id": 6,
+        "name": "СМИ",
+        "src": "svg/orders/media.svg"
+    },
+
+    {
+        "id": 7,
+        "name": "Разработка ПО",
+        "src": "svg/orders/it.svg"
+    },
+
+    {
+        "id": 8,
+        "name": "Проектирование",
+        "src": "svg/orders/engineering.svg"
+    },
+
+    {
+        "id": 9,
+        "name": "Дизайн",
+        "src": "svg/orders/design.svg"
+    },
+
+    {
+        "id": 10,
+        "name": "Финансы и бухгалтерия",
+        "src": "svg/orders/finance.svg"
+    },
+
+    {
+        "id": 11,
+        "name": "HoReCa",
+        "src": "svg/orders/horeca.svg"
+    },
+    {
+        "id": 12,
+        "name": "Маркетинг",
+        "src": "svg/orders/marketing.svg"
+    },
+    {
+        "id": 13,
+        "name": "Красота и здоровье",
+        "src": "/svg/orders/beauty.svg"
+    },
+]
+
+ORDERS = [
+    {
+        'id': i,
+        'catalog_id': random.randint(1, 13),
+        'title': f'Работа такая-то № {i}',
+        'owner': f'Company{i}',
+        'ownerPicture': 'img/user1.jpg',
+        'ownerShortInfo': f'blablalba{i}',
+        'ownerPlace': f'office{i}',
+        'period': f'ii.ii.iii{i}',
+        'job': f'job {i}',
+        'place': f'place{i}',
+        'salary': f'{i * i}',
+        'contract': f'contract{i}',
+        'condition': f'no',
+        'aboutDeal': f'you need to do it{i}'
+    } for i in range(150)
+]
+
+CONTRACTORS = [
+    {
+        'id': i,
+        'catalog_id': random.randint(1, 13),
+        'name': f'Name{i}',
+        'picture': 'img/user1.jpg',
+        'job': f'job {i}',
+        'another_skills': f'job-job{i}',
+        'age': f'1{i}',
+        'sex': 'male',
+        'citship': f'citizenship{i}',
+        'townJob': f'Town{i}',
+        'languages': 'English',
+        'another_townJob': f'Town Another {i}',
+        'salary': f'{i * i}',
+        'medBook': 'Yes',
+        'contraindications': 'None',
+        'schedule': '1/3',
+    } for i in range(10)
+]
--- webapp/views.py
+++ webapp/views.py
@@ -1,33 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from webapp.database import CATALOGS, ORDERS, CONTRACTORS
+import random
 
 
 # Заказы
 
+
 def home(req):
-    return render(req, 'public/pages/orders_mainPage.html')
+    return render(req, 'public/pages/orders_mainPage.html', {
+        'catalogs': CATALOGS
+    })
 
 
-def orders(req):
-    return render(req, 'public/pages/orders_listing.html')
+def orders(req):  # тут нужно дописать логику с get-параметрами, идущими в качестве параметров
+    catalog_id = int(req.GET.get('catalog_id', -1))
+    items = [item for item in ORDERS if item['catalog_id'] == catalog_id]
+    print(items)
+    return render(req, 'public/pages/orders_listing.html', {
+        'orders': items
+    })
 
 
-def order(req):
-    return render(req, 'public/order.html')
+def order(req, order_id):  # тут нужно дописать логику с get-параметрами, идущими в качестве параметров
+    selected_order = next((item for item in ORDERS if item['id'] == order_id), -1)
+    return render(req, 'public/pages/order.html', {
+        'order': selected_order
+    })
 
 
 # Исполнители
+
 
 def executors(req):
     return render(req, 'public/pages/executors_mainPage.html')
 
 
-def contractors(req):
+def contractors(req):  # тут нужно дописать логику с get-параметрами, идущими в качестве параметров
     return render(req, 'public/pages/executors_listing.html')
 
 
-def contractor(req):
-    return render(req, 'public/executor.html')
-
-
-
+def contractor(req):  # тут нужно дописать логику с get-параметрами, идущими в качестве параметров
+    return render(req, 'public/pages/executor.html')
